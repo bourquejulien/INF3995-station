598f8be7150ec1c2f972f6b899b02e2e741ef5a6
Julien Bourque <julien.bourque@polymtl.ca>
2022-09-14 13:51:59 -0400

    Added simulation address env variable

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
+from os import environ as env
+
 config = {
     "argos_url": {
-        "host": "127.0.0.1",
+        "host": env.get("SIMULATION_ADDR") if env.get("SIMULATION_ADDR") is not None else "localhost",
         "port": 9854
     }
 }
