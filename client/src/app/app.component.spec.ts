01488d5cab41e5aeaf84696de00bb6e99a6a4896
Julien Bourque <julien.bourque@polymtl.ca>
2022-09-21 19:59:39 -0400

    Added testing to Dockerfile.

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,6 +8,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [HttpClientModule]
     }).compileComponents();
   });
 
@@ -20,12 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('web-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('web-ui app is running!');
   });
 });
--- client/src/app/services/command.service.spec.ts
+++ client/src/app/services/command.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CommandService } from './command.service';
+import {HttpClientModule} from "@angular/common/http";
 
 describe('CommandService', () => {
   let service: CommandService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientModule]
+    });
     service = TestBed.inject(CommandService);
   });
 
