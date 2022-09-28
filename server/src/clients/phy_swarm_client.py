2e31aea530961d2f021cf6e557c28be8ee8ae1d2
Julien Bourque <julien.bourque@polymtl.ca>
2022-09-28 12:43:51 -0400

    =Resolved MR comments

--- server/src/clients/phy_swarm_client.py
+++ server/src/clients/phy_swarm_client.py
@@ -1,5 +1,4 @@
 import logging
-import time
 
 import cflib
 from cflib import crtp
--- server/src/services/persistent_service.py
+++ server/src/services/persistent_service.py
@@ -3,14 +3,14 @@ from src.clients.swarm_client import SwarmClient
 
 class PersistentService:
     def __init__(self, swarm_client: SwarmClient):
-        self._swarmCLient = swarm_client
+        self._swarm_client = swarm_client
         self.drones_ids = []
 
     def start(self):
-        self.drones_ids = self._swarmCLient.discover()
+        self.drones_ids = self._swarm_client.discover()
 
     def connect(self, uris):
-        self._swarmCLient.connect(uris)
+        self._swarm_client.connect(uris)
 
     def disconnect(self):
-        self._swarmCLient.disconnect()
+        self._swarm_client.disconnect()
