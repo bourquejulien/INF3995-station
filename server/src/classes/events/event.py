bff7c0601f52f15a1f0ee6ac6b18968d3e31b6df
Julien Bourque <julien.bourque@polymtl.ca>
2022-10-20 23:25:16 -0400

    Added dataclasses

---
+++ server/src/classes/events/event.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime, timezone
+from typing import Optional
+
+
+def get_timestamp_ms():
+    return int(datetime.now(timezone.utc).timestamp() * 1000)
+
+
+@dataclass
+class Event:
+    id: Optional[str]
+
+    def to_json(self):
+        data = asdict(self)
+        if self.id is not None:
+            data["_id"] = self.id
+
+        data.pop("id")
+
+        return data
+
+    def __str__(self):
+        return self.to_json().__str__()
---
+++ server/src/classes/events/log.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from src.classes.events.event import Event, get_timestamp_ms
+
+
+def generate_log(mission_id: str, message: str, level: int):
+    return Log(None, get_timestamp_ms(), mission_id, message, level)
+
+
+@dataclass
+class Log(Event):
+    timestamp_ms: int
+    mission_id: str
+    message: str
+    level: int
