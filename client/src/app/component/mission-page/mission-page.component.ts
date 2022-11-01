8aea1a1219f9941fc1bead057501215f1f38f0af
Gabriel Bruno <gabriel.bruno@polymtl.ca>
2022-11-01 13:24:45 -0400

    canvas resizes with page

--- client/src/app/component/mission-page/mission-page.component.ts
+++ client/src/app/component/mission-page/mission-page.component.ts
@@ -9,26 +9,28 @@ import { CommandService } from '@app/services/command/command.service';
 })
 export class MissionPageComponent implements OnInit {
     @ViewChild('canvas', { static: false }) canvas!: ElementRef;
+    @ViewChild('colMission', { static: false }) colMission!: ElementRef;
 
     constructor(private router: Router, public commandService: CommandService) {
         this.selectedUris = [];
-        this.map = null;
+        this.mapContext = null;
+        this.pixelSize = 6;
     }
 
     selectedUris: string[];
-    map: CanvasRenderingContext2D | null;
+    mapContext: CanvasRenderingContext2D | null;
+    pixelSize: number;
 
     ngOnInit(): void {
         this.commandService.discover();
         this.commandService.retrieveMode();
+        window.addEventListener("resize", this.resizeMap.bind(this), false);
     }
     
     ngAfterViewInit(): void {
-        this.map = this.canvas.nativeElement.getContext("2d") as CanvasRenderingContext2D;
-        // this.map.fillStyle = "green";
-        // this.map.fillRect(10, 10, 100, 100);
-        //this.map.fillStyle = "green";
-        this.map.fillRect(10, 10, 100, 100);
+        this.mapContext = this.canvas.nativeElement.getContext("2d") as CanvasRenderingContext2D;
+        this.resizeMap();
+        
     }
 
     identify(): void {
@@ -61,5 +63,35 @@ export class MissionPageComponent implements OnInit {
             return;
         }
         this.selectedUris.splice(uriPosition, 1);
+    }
+
+    drawPixel(X: number, Y: number): void {
+        let Xpixelized = Math.floor(X / this.pixelSize) * this.pixelSize; // Rounds to the previous multiple of the pixel size
+        let Ypixelized = Math.floor(Y / this.pixelSize) * this.pixelSize;
+        this.mapContext!.fillRect(Xpixelized, Ypixelized, this.pixelSize, this.pixelSize);
+    }
+
+    drawCircle(Xcenter: number, Ycenter: number, radius: number): void {
+        let iterations = 200;
+        for (let i = 0; i < iterations; i++) {
+            let angle = (i / iterations) * 2 * Math.PI;
+            let x = Xcenter + radius * Math.cos(angle);
+            let y = Ycenter + radius * Math.sin(angle);
+            this.drawPixel(x, y);
+        }
+    }
+
+    resizeMap(): void {
+        let canvasSize = Math.min(this.colMission.nativeElement.offsetWidth * 0.9, (this.colMission.nativeElement.offsetHeight - 50) * 0.9);
+
+        this.canvas.nativeElement.width = canvasSize;
+        this.canvas.nativeElement.height = canvasSize;
+
+        let width = this.canvas.nativeElement.width;
+        let height = this.canvas.nativeElement.height;
+        this.mapContext!.fillStyle = "white";
+        this.mapContext!.fillRect(0, 0, width, height);
+        this.mapContext!.fillStyle = "black";
+        this.drawCircle(150, 200, 145);
     }
 }
