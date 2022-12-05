870bbafe737b64aab9164ddc5824ab515eac8933
TSpoons <thierry.spooner@polymtl.ca>
2022-12-05 15:33:26 -0500

    --amend

--- client/src/app/services/command/command.service.ts
+++ client/src/app/services/command/command.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '@environment';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -17,12 +19,14 @@ export class CommandService {
     async connect(): Promise<void> {
         this.uris = await this.httpClient
             .post<string[]>(`${environment.serverURL}/discovery/connect`, undefined)
+            .pipe(catchError(this.handleError))
             .toPromise();
     }
 
     async disconnect(): Promise<void> {
         await this.httpClient
             .post(`${environment.serverURL}/discovery/disconnect`, undefined)
+            .pipe(catchError(this.handleError))
             .toPromise();
         this.uris = [];
     }
@@ -30,24 +34,33 @@ export class CommandService {
     async identify(uris: string[]): Promise<void> {
         await this.httpClient
             .post(`${environment.serverURL}/action/identify`, uris)
+            .pipe(catchError(this.handleError))
             .toPromise();
     }
 
     async toggleSync(): Promise<void> {
         await this.httpClient
             .post(`${environment.serverURL}/action/toggle_sync`, null)
+            .pipe(catchError(this.handleError))
             .toPromise();
     }
 
     async getUris(): Promise<void> {
         this.uris = await this.httpClient
             .get<string[]>(`${environment.serverURL}/discovery/uris`)
+            .pipe(catchError(this.handleError))
             .toPromise();
     }
 
     async retrieveMode(): Promise<void> {
         this.isSimulation = await this.httpClient
             .get<boolean>(`${environment.serverURL}/is_simulation`)
+            .pipe(catchError(this.handleError))
             .toPromise();
+    }
+
+    private handleError(error: HttpErrorResponse): Observable<never> {
+        if (error.status === 0) return throwError(new Error('Server is unavailable'));
+        return throwError(error);
     }
 }
