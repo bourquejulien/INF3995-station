6c04e317a95b03fda271294210dd6aa493e48b55
Audrey Leblanc <audrey-1.leblanc@polymtl.ca>
2022-12-07 15:06:49 +0000

    Feature/map database

---
+++ server/src/classes/events/mapDatabase.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+from src.classes.events.event import Event, get_timestamp_ms
+from src.classes.vec2 import Vec2
+
+def generate_mapDatabase(positionObstacle: list[Vec2], mission_id: str = ""):
+    return MapDatabase(None, positionObstacle, mission_id)
+
+@dataclass
+class MapDatabase(Event):
+    obstaclePosition: list[Vec2]
+    mission_id: str
